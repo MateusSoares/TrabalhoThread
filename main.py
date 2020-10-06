769b384a9c8af9dab7f002b90ec78ce3dbce0300
Mateus Soares <fvs.mateus@gmail.com>
2020-10-06 15:34:01 -0300

    m - finalizado

--- main.py
+++ main.py
@@ -81,5 +81,5 @@ if __name__ == '__main__':
 
     for i in range(1, prod_max+1):
         threading.Thread(target=produtor, args=(i,)).start()
-    for i in range(1, prod_max+1):
+    for i in range(1, cons_max+1):
         threading.Thread(target=consumidor, args=(i,)).start()
